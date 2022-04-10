7938f57f70b906bb36a351f475b2701aa6b4250e
JHessler <jhessler11@gmail.com>
2022-04-10 14:24:50 -0400

    model templates added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+// links to the main controllers
 const routes = require('./controllers')
 
 const app = express();
@@ -26,9 +27,7 @@ const sess = {
 app.use(session(sess));
 
 const helpers = require('./utils/helpers');
-
 const hbs = exphbs.create({ helpers });
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
