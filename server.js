a8cd9c6b4fe950c0a65ef96414c8f2f602acd721
JHessler <jhessler11@gmail.com>
2022-04-10 13:18:45 -0400

    fixed routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,9 +10,12 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+//connect to sequelize database
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 1000000
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -32,7 +36,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(require('./controllers/'));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
