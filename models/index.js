e7f0fd83ea3b08e47370f4f1d338326bd3b79cb0
JHessler <jhessler11@gmail.com>
2022-04-16 10:51:08 -0400

    added associations

--- models/index.js
+++ models/index.js
@@ -3,6 +3,29 @@ const Comments = require('./Comments');
 const Post = require("./Post")
 
 
-//future associations 
+//associations 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+  Comments.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Comments.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Comments, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Comments, {
+    foreignKey: 'post_id'
+  });
 
 module.exports = { User, Comments, Post };
